ca3900fd64ecee85eb78ed995f574a57277add28
Amira <a.elsayed@praxilabs.com>
2025-05-31 14:40:44 +0300

    Strategy Pattern

---
+++ Strategy Pattern/Strategy Pattern/Invoice.cs
@@ -0,0 +1,12 @@
+﻿namespace Strategy_Pattern;
+public class Invoice
+{
+    public Customer Customer { get; set; }
+    public double TotalPrice { get; set; }
+    public double NetPrice { get; set; }
+    public List<InvoiceLine> InvoiceLines{ get; set; }
+    public override string ToString()
+    {
+        return $"Total Price: {TotalPrice}, Net Price: {NetPrice}";
+    }
+}
---
+++ Strategy Pattern/Strategy Pattern/Strategies/IDiscountStrategy.cs
@@ -0,0 +1,5 @@
+﻿namespace Strategy_Pattern.Strategies;
+public interface IDiscountStrategy
+{
+    double CalculateDiscount(double totalPrice);
+}
